a6818babcedab2fe5cdd7d83129fd73945ff2c11
BartvLaatum <hbpvanlaatum123@gmail.com>
2025-04-22 18:07:59 +0200

    added solver-timing to smpc

--- experiments/horizon_smpc.py
+++ experiments/horizon_smpc.py
@@ -8,8 +8,8 @@ from tqdm import tqdm
 import numpy as np
 
 from common.results import Results
-from common.utils import get_parameters
-from rl_smpc import create_rl_smpc, load_experiment_parameters, Experiment
+from common.utils import get_parameters, load_env_params, load_mpc_params
+from smpc import SMPC, Experiment
 
 N_SIMS = 10
 
@@ -18,7 +18,6 @@ def main(args):
 
     save_path = f"data/{args.project}/stochastic/rlsmpc"
     os.makedirs(save_path, exist_ok=True)
-
 
     col_names = [
         "time", "x_0", "x_1", "x_2", "x_3", "y_0", "y_1", "y_2", "y_3",
@@ -30,20 +29,14 @@ def main(args):
 
     # run the experiment
     print(f"Running experiment for delta = {args.uncertainty_value}")
-    (env_params, mpc_params, rl_env_params, env_path, rl_model_path, vf_path) = \
-        load_experiment_parameters(
-            args.project, 
-            args.env_id, 
-            args.algorithm, 
-            args.mode, 
-            args.model_name, 
-            args.uncertainty_value)
+    env_params = load_env_params(args.env_id)
+    mpc_params = load_mpc_params(args.env_id)
+    mpc_params["Ns"] = 10
 
     for h in H:
         results = Results(col_names)
         print(f"Running for prediction horizon {h} hours")
-        save_name = f"{args.model_name}-{args.save_name}-{h}H-{args.uncertainty_value}.csv"
-
+        save_name = f"{args.save_name}-{h}H-{args.uncertainty_value}.csv"
         p = get_parameters()
         run_exp = partial(
             run_experiment, 
@@ -51,11 +44,7 @@ def main(args):
             uncertainty_value=args.uncertainty_value,
             env_params=env_params,
             mpc_params=mpc_params,
-            rl_env_params=rl_env_params,
             args=args,
-            env_path=env_path,
-            rl_model_path=rl_model_path,
-            vf_path=vf_path,
             p=p,
             save_name=save_name, 
         )
@@ -74,33 +63,24 @@ def run_experiment(
         uncertainty_value,
         env_params, 
         mpc_params,
-        rl_env_params,
         args,
-        env_path,
-        rl_model_path,
-        vf_path,
         p,
         save_name,
     ):
+
     # Add a small delay based on run ID to avoid resource contention
+    mpc_rng = np.random.default_rng(42 + run)
+    save_name = f"{args.save_name}-{h}H-{mpc_params['Ns']}Ns-{args.uncertainty_value}"
+    mpc_params["rng"] = mpc_rng
 
-    rl_mpc = create_rl_smpc(
-        h, 
-        env_params, 
-        mpc_params,
-        rl_env_params,
-        args.algorithm,
-        env_path,
-        rl_model_path,
-        vf_path,
-        run,
-        args.use_trained_vf
-    )
+    # p = DefineParameters()
+    p = get_parameters()
+    smpc = SMPC(**env_params, **mpc_params)
 
-    rl_mpc.define_zero_order_snlp(p)
+    smpc.define_nlp(p)
     exp_rng = np.random.default_rng(666 + run)
-    exp = Experiment(rl_mpc, save_name, args.project, args.weather_filename, uncertainty_value, p, exp_rng)
-    exp.solve_nsmpc("zero")
+    exp = Experiment(smpc, save_name, args.project, args.weather_filename, uncertainty_value, p, exp_rng)
+    exp.solve_nmpc()
     return exp.get_results(run)
 
 if __name__ == "__main__":
@@ -111,12 +91,9 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--project", type=str, default="uncertainty-comparison")
     parser.add_argument("--env_id", type=str, default="LettuceGreenhouse")
-    parser.add_argument("--model_name", type=str, default="thesis-agent")
     parser.add_argument("--save_name", type=str)
     parser.add_argument("--weather_filename", default="outdoorWeatherWurGlas2014.csv", type=str)
-    parser.add_argument("--algorithm", type=str, default="sac")
     parser.add_argument("--mode", type=str, choices=['deterministic', 'stochastic'], required=True)
     parser.add_argument("--uncertainty_value", type=float, help="List of uncertainty values")
-    parser.add_argument("--use_trained_vf", action="store_true")
     args = parser.parse_args()
     main(args)
