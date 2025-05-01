e3fb069a34b0d2891caa41247940a73e39c2b13d
BartvLaatum <hbpvanlaatum123@gmail.com>
2025-05-01 16:35:03 +0200

    bash script for tuning weather generalization

--- run_scripts/rl_weather_gen.sh
+++ run_scripts/rl_weather_gen.sh
@@ -7,11 +7,11 @@ echo "PYTHONPATH set to: $PYTHONPATH"
 PROJECT="Weather-Generalization"
 ENV_ID="LettuceGreenhouse"
 ALGORITHM="sac"
-N_EVAL_EPISODES=10
-N_EVALS=10
+N_EVAL_EPISODES=5
+N_EVALS=8
 MODE="stochastic"
 uncertainty_values=(0.1)
-training_years=(2005 2006 2007 2008 2009 2010)
+training_years=(2001 2002 2003 2004 2005 2006 2007 2008 2009 2010)
 
 for uncertainty_value in "${uncertainty_values[@]}"; do
     echo "Training with uncertainty value $uncertainty_value..."
@@ -24,6 +24,7 @@ for uncertainty_value in "${uncertainty_values[@]}"; do
         --mode $MODE \
         --uncertainty_value $uncertainty_value \
         --save_model \
-        --training_years 2005 2006 2007 2008 2009 2010 \
+        --training_years ${training_years[@]} \
+        --hyperparameter_tuning \
         --save_env
 done
