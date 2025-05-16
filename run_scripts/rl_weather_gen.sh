1706100922ed46cdbeee4af6bea31754d22543c8
BartvLaatum <hbpvanlaatum123@gmail.com>
2025-05-16 12:13:07 +0200

    script to test weather generalization

--- run_scripts/rl_weather_gen.sh
+++ run_scripts/rl_weather_gen.sh
@@ -8,7 +8,7 @@ PROJECT="Weather-Generalization"
 ENV_ID="LettuceGreenhouse"
 ALGORITHM="sac"
 N_EVAL_EPISODES=5
-N_EVALS=8
+N_EVALS=20
 MODE="stochastic"
 uncertainty_values=(0.1)
 training_years=(2001 2002 2003 2004 2005 2006 2007 2008 2009 2010)
@@ -25,6 +25,6 @@ for uncertainty_value in "${uncertainty_values[@]}"; do
         --uncertainty_value $uncertainty_value \
         --save_model \
         --training_years ${training_years[@]} \
-        --hyperparameter_tuning \
         --save_env
+        # --hyperparameter_tuning \
 done
