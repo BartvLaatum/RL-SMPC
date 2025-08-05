4cfdcf08c662f94bee02450f46900b087123db69
BartvLaatum <hbpvanlaatum123@gmail.com>
2025-08-05 16:36:13 +0200

    Bash script to train RL agents with varying parametric model uncertainty

--- run_scripts/train_stoch_rl.sh
+++ run_scripts/train_stoch_rl.sh
@@ -4,13 +4,13 @@ echo "PYTHONPATH set to: $PYTHONPATH"
 # Script to train RL agents with different uncertainty values
 
 # Common arguments
-PROJECT="SMPC"
-ENV_ID="LettuceGreenhouse"
+PROJECT="RL-SMPC"
+ENV_ID="LettuceGreenhouseStateNoise"
 ALGORITHM="sac"
-N_EVAL_EPISODES=10
-N_EVALS=10
+N_EVAL_EPISODES=20
+N_EVALS=20
 MODE="stochastic"
-uncertainty_values=(0.1)
+uncertainty_values=(0.025 0.05 0.075 0.1 0.125 0.15 0.175 0.2)
 
 for uncertainty_value in "${uncertainty_values[@]}"; do
 echo "Training with uncertainty value $uncertainty_value..."
