201554c05e8fba298ade0214895e43b9d69835a8
BartvLaatum <hbpvanlaatum123@gmail.com>
2025-10-08 14:57:13 +0200

    added computational complexity experiments, and various seeds

--- run_scripts/train_vf.sh
+++ run_scripts/train_vf.sh
@@ -8,26 +8,27 @@ ENV_ID="LettuceGreenhouse"
 ALGORITHM="sac"
 MODE="stochastic"
 UNCERTAINTY_VALUE=0.1
-MODEL_NAME="brisk-resonance-24"
+# MODEL_NAMEDS=("brisk-resonance-24")
+MODEL_NAMES=("iconic-dust-9" "eager-bee-10" "logical-forest-11" "swift-armadillo-12")
 
 # First run value function training
-echo "Training value function..."
-python RL/vf_TR_learning.py \
-    --project $PROJECT \
-    --model_name $MODEL_NAME \
-    --env_id $ENV_ID \
-    --algorithm $ALGORITHM \
-    --uncertainty_value $UNCERTAINTY_VALUE \
-    --mode $MODE
+for MODEL_NAME in "${MODEL_NAMES[@]}"; do
+    echo "Training value function, for model: $MODEL_NAME..."
+    python RL/vf_TR_learning.py \
+        --project $PROJECT \
+        --model_name $MODEL_NAME \
+        --env_id $ENV_ID \
+        --algorithm $ALGORITHM \
+        --uncertainty_value $UNCERTAINTY_VALUE \
+        --mode $MODE
 
-# Evaluate RL agent
-echo "Evaluate RL agent..."
-python RL/evaluate_rl.py \
-    --project $PROJECT \
-    --model_name $MODEL_NAME \
-    --env_id $ENV_ID \
-    --algorithm $ALGORITHM \
-    --mode $MODE \
-    --uncertainty_value $UNCERTAINTY_VALUE
-
-echo "Done!"
+    # Evaluate RL agent
+    echo "Evaluating RL agent with named: $MODEL_NAME..."
+    python RL/evaluate_rl.py \
+        --project $PROJECT \
+        --model_name $MODEL_NAME \
+        --env_id $ENV_ID \
+        --algorithm $ALGORITHM \
+        --mode $MODE \
+        --uncertainty_value $UNCERTAINTY_VALUE
+done
